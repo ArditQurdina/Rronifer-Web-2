27e1f108d25d5e3a50a979d3daad771dcaee0026
armir00 <armir@bbros.eu>
2026-05-11 20:18:50 +0200

    next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['indebtedly-yeld-marcy.ngrok-free.dev'],  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
