041ceb2080d465900fbca910488064fd6b4c5df9
armir00 <armir@bbros.eu>
2026-05-13 10:49:43 +0200

    hover

--- app/about-us/ForceBehindLandmarks.tsx
+++ app/about-us/ForceBehindLandmarks.tsx
@@ -36,7 +36,7 @@ export default function ForceBehindLandmarks() {
                         </p>
                         <Link
                             href="/contact-us#hear-from-you"
-                            className="w-fit px-6 md:px-9 py-3 md:py-4 border border-[#3F3F46] text-white uppercase tracking-[0.3em] text-[11px] font-extrabold flex items-center justify-center"
+                            className="w-fit px-6 md:px-9 py-3 md:py-4 border border-[#3F3F46] text-white uppercase tracking-[0.3em] text-[11px] font-extrabold flex items-center justify-center hover:bg-[#111111]"
                         >
                             {t('about.requestDetail')}
                         </Link>
